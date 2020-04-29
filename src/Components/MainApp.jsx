c9e962d42d7caf8dddd3d70fb844352e9228a87c
Justine810 <justine.gautreau@gmail.com>
2020-04-29 15:52:29 +0200

    crea composants mainapp et title

---
+++ src/Components/MainApp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './MainApp.css';
+import Title from './Title'; 
+
+function MainApp() {
+  return (
+    <div className="background">
+      <Title />
+    </div>
+  );
+}
+
+export default MainApp;
